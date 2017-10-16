adcd68538dc64937fe0b074f5a5af993ccbf8142
alejandro <victor.saico@tecsup.edu.pe>
2017-10-16 17:09:50 -0500

    VersionLunes

--- app/src/main/java/com/victorsaico/shareddrawer/Activities/LoginActivity.java
+++ app/src/main/java/com/victorsaico/shareddrawer/Activities/LoginActivity.java
@@ -1,18 +1,20 @@
 package com.victorsaico.shareddrawer.Activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.victorsaico.shareddrawer.Models.User;
 import com.victorsaico.shareddrawer.R;
 import com.victorsaico.shareddrawer.Repositories.UserRepository;
+import com.victorsaico.shareddrawer.models.User;
 
 public class LoginActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
@@ -20,7 +22,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText usernameInput;
     private EditText passwordInput;
     private ProgressBar progressBar;
+    private String email,password,fullname;
     private View loginPanel;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,40 +50,69 @@ public class LoginActivity extends AppCompatActivity {
         loginPanel.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
 
-        String username = usernameInput.getText().toString();
-        String password = passwordInput.getText().toString();
+        email = usernameInput.getText().toString();
+        password = passwordInput.getText().toString();
 
-        if(username.isEmpty() || password.isEmpty()){
+        if(email.isEmpty() || password.isEmpty()){
             Toast.makeText(this, "You must complete these fields", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Login logic
-        User user = UserRepository.login(username, password);
-
+        //Login logic
+        user = UserRepository.login(email, password);
         if(user == null){
-            user = UserRepository.agregarUsuario(username, password);
+            //UserRepository.create(email, email, password);
+            showDialog();
             loginPanel.setVisibility(View.VISIBLE);
             progressBar.setVisibility(View.GONE);
             return;
         }
 
-        Toast.makeText(this, "Welcome " + user.getFullname(), Toast.LENGTH_SHORT).show();
-
-        // Save to SharedPreferences
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        boolean success = editor
-                .putString("username", user.getUsername())
-                .putString("fullname",user.getFullname())
-                .putBoolean("islogged", true)
-                .commit();
-
-        // Go to Dashboard
-        goDashboard();
+        guardarPreferences();
     }
     private void goDashboard(){
         startActivity(new Intent(this, HomeActivity.class));
         finish();
     }
+    public void showDialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_regiser_dialog);
+        dialog.setTitle("Registrar Usuario");
+
+        final EditText edtfullname = (EditText) dialog.findViewById(R.id.edtfullname);
+        Button btnregistrar = (Button) dialog.findViewById(R.id.btnregistrar);
+        Button btncancelar = (Button) dialog.findViewById(R.id.btncancelar);
+
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fullname = edtfullname.getText().toString();
+                user = UserRepository.create(email, fullname,password);
+                guardarPreferences();
+                return;
+            }
+        });
+        btncancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+     }
+     public void guardarPreferences(){
+         Toast.makeText(this, "Welcome " + user.getFullname(), Toast.LENGTH_SHORT).show();
+
+         // Save to SharedPreferences
+         SharedPreferences.Editor editor = sharedPreferences.edit();
+         boolean success = editor
+                 .putString("username", user.getFullname())
+                 .putString("fullname",user.getFullname())
+                 .putBoolean("islogged", true)
+                 .commit();
+
+         // Go to Dashboard
+         goDashboard();
+     }
 
 }
--- app/src/main/java/com/victorsaico/shareddrawer/Repositories/UserRepository.java
+++ app/src/main/java/com/victorsaico/shareddrawer/Repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.victorsaico.shareddrawer.Repositories;
 
-import com.victorsaico.shareddrawer.Models.User;
+import com.orm.SugarRecord;
+import com.victorsaico.shareddrawer.models.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,36 +11,46 @@ import java.util.List;
 
 public class UserRepository {
 
-    public static List<User> users = new ArrayList<>();
+    //public static List<User> users = new ArrayList<>();
 
-    static{
-        users.add(new User(100, "ebenites", "tecsup", "Erick Benites"));
-        users.add(new User(200, "jfarfan", "tecsup", "Jaime Farfán"));
-        users.add(new User(300, "drodriguez", "tecsup", "David Rodriguez"));
-    }
+  //  static{
+     //   users.add(new User(100, "ebenites", "tecsup", "Erick Benites"));
+    //    users.add(new User(200, "jfarfan", "tecsup", "Jaime Farfán"));
+     //   users.add(new User(300, "drodriguez", "tecsup", "David Rodriguez"));
+    //}
 
-    public static User login(String username, String password){
-        for (User user : users){
-            if(user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)){
-                return user;
-            }
-        }
-        return null;
-    }
+   public static List<User> users = SugarRecord.listAll(User.class);
+   public static User login(String email, String password) {
+       for (User user : users) {
+           if (user.getFullname().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
+               return user;
+           }
+       }
+       return null;
+   }
 
-    public static User getUser(String username){
-        for (User user : users){
-            if(user.getUsername().equalsIgnoreCase(username)){
-                return user;
-            }
-        }
-        return null;
-    }
-    public static User agregarUsuario(String username, String password){
-        int id=100*(users.size()+1);
-        User user = new User(id, username, password, username);
-        users.add(user);
+  //  public static User getUser(String username){
+    //    for (User user : users){
+         //   if(user.getUsername().equalsIgnoreCase(username)){
+           //     return user;
+         //   }
+      ///  }
+     //  return null;
+    //}
+    //public static User agregarUsuario(String username, String password){
+      //  int id=100*(users.size()+1);
+       // User user = new User(id, username, password, username);
+     //   users.add(user);
+    //    return user;
+   // }
+  public static User read(Long id){
+      User user = SugarRecord.findById(User.class, id);
+      return user;
+  }
+
+    public static User create(String fullname, String email, String password){
+        User user = new User(fullname, email, password);
+        SugarRecord.save(user);
         return user;
     }
-
 }
